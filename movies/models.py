0572975af75d4a9e294a3af804518d6e436a1cf2
SelvinAnibalToledo <144059429+SelvinAnibalToledo@users.noreply.github.com>
2023-10-04 21:22:56 -0700

    Update models.py

--- movies/models.py
+++ movies/models.py
@@ -1,16 +1,45 @@
 from django.db import models
 
-class Studio(models.Model):
-    name = models.CharField(max_length=30)
+class Estudio(models.Model):
+    estudio = models.TextField()
+class Genero(models.Model):
+    genero = models.TextField()
 
-class Movie(models.Model):
-    title = models.CharField(max_length=100)
-    release_date = models.DateField()
-    country = models.CharField(max_length=60)
-    description= models.TextField()
-    GenreType= models.TextChoices("GenreType", "Adventure Drama Sci-Fi Comedy Action Fantasy Horror")
-    genre = models.CharField(blank=False, choices=GenreType.choices, max_length=18)
-    studio = models.ForeignKey(Studio,on_delete=models.SET_NULL, null=True)
+class Persona(models.Model):
+    Nombre = models.TextField()
+    Fecha_Nac = models.DateField()
+    Pais = models.TextField()
+    Url_imagen = models.ImageField()
+    
+class Pelicula(models.Model):
+    titulo = models.CharField(max_length=200)
+    anio = models.IntegerField()
+    pais = models.TextField()
+    estreno = models.TextField()
+    descripcion = models.TextField()
+    duracion = models.IntegerField()
+    gross = models.CharField(max_length=500)
+    velimasen = models.CharField(max_length=500)
+    estudio = models.ForeignKey(Estudio,on_delete=models.CASCADE)
+    genero = models.ForeignKey(Genero,on_delete=models.CASCADE)
+    
+class Rol(models.Model):
+    pelicula = models.ForeignKey(Pelicula,on_delete=models.CASCADE)   
+    persona = models.ForeignKey(Persona,on_delete=models.CASCADE)  
+    
+class Clasificacion(models.Model)
+    clasificacion = models.TextField()
 
-class Prueba(models.Model):
-    name = models.CharField(max_length=30)
+class Resena(models.Model):
+    texto = models.TextField()
+    calificacion = models.IntegerField()
+    pelicula = models.ForeignKey(Pelicula,on_delete=models.CASCADE)   
+    usuario = models.ForeignKey(Usuario,on_delete=models.CASCADE)   
+
+class Usuario(models.Model):
+    nombres = models.TextField()
+    correo = models.TextField()
+    seguidor = models.ForeignKey(Seguidor,on_delete=models.CASCADE) 
+
+class Seguidor(models.Model):
+    usuario = models.ForeignKey(Usuario,on_delete=models.CASCADE)   
