d492bef7b7c5caf11fd6ba5ae356961a14752754
SelvinAnibalToledo <Selvin3901@gmail.com>
2023-10-05 04:48:00 +0000

    sobreescribir metodo de modelos

--- movies/models.py
+++ movies/models.py
@@ -2,14 +2,21 @@ from django.db import models
 
 class Estudio(models.Model):
     estudio = models.TextField()
+
+    def __str__(self):
+        return self.estudio
+
 class Genero(models.Model):
     genero = models.TextField()
+    
+    def __str__(self):
+        return self.genero
 
 class Persona(models.Model):
     Nombre = models.TextField()
     Fecha_Nac = models.DateField()
     Pais = models.TextField()
-    Url_imagen = models.ImageField()
+ #   Url_imagen = models.ImageField()
     
 class Pelicula(models.Model):
     titulo = models.CharField(max_length=200)
@@ -27,8 +34,12 @@ class Rol(models.Model):
     pelicula = models.ForeignKey(Pelicula,on_delete=models.CASCADE)   
     persona = models.ForeignKey(Persona,on_delete=models.CASCADE)  
     
-class Clasificacion(models.Model)
+class Clasificacion(models.Model):
     clasificacion = models.TextField()
+
+class Usuario(models.Model):
+    nombres = models.TextField()
+    correo = models.TextField()
 
 class Resena(models.Model):
     texto = models.TextField()
@@ -36,10 +47,5 @@ class Resena(models.Model):
     pelicula = models.ForeignKey(Pelicula,on_delete=models.CASCADE)   
     usuario = models.ForeignKey(Usuario,on_delete=models.CASCADE)   
 
-class Usuario(models.Model):
-    nombres = models.TextField()
-    correo = models.TextField()
-    seguidor = models.ForeignKey(Seguidor,on_delete=models.CASCADE) 
-
-class Seguidor(models.Model):
-    usuario = models.ForeignKey(Usuario,on_delete=models.CASCADE)   
+#class Seguidor(models.Model):
+#    usuario = models.ForeignKey(Usuario,on_delete=models.CASCADE)   
