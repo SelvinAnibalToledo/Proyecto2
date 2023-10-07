209daf1b2085df83f183effcae8042d018b400b8
SelvinAnibalToledo <144059429+SelvinAnibalToledo@users.noreply.github.com>
2023-10-06 20:20:06 -0700

    Update models.py

--- movies/models.py
+++ movies/models.py
@@ -1,51 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-class Estudio(models.Model):
-    estudio = models.TextField()
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.estudio
+        return self.name
 
-class Genero(models.Model):
-    genero = models.TextField()
-    
+
+class Job(models.Model):
+    name = models.CharField(max_length=200)
+
     def __str__(self):
-        return self.genero
+        return self.name
 
-class Persona(models.Model):
-    Nombre = models.TextField()
-    Fecha_Nac = models.DateField()
-    Pais = models.TextField()
- #   Url_imagen = models.ImageField()
-    
-class Pelicula(models.Model):
-    titulo = models.CharField(max_length=200)
-    anio = models.IntegerField()
-    pais = models.TextField()
-    estreno = models.TextField()
-    descripcion = models.TextField()
-    duracion = models.IntegerField()
-    gross = models.CharField(max_length=500)
-    velimasen = models.CharField(max_length=500)
-    estudio = models.ForeignKey(Estudio,on_delete=models.CASCADE)
-    genero = models.ForeignKey(Genero,on_delete=models.CASCADE)
-    
-class Rol(models.Model):
-    pelicula = models.ForeignKey(Pelicula,on_delete=models.CASCADE)   
-    persona = models.ForeignKey(Persona,on_delete=models.CASCADE)  
-    
-class Clasificacion(models.Model):
-    clasificacion = models.TextField()
 
-class Usuario(models.Model):
-    nombres = models.TextField()
-    correo = models.TextField()
+class Person(models.Model):
+    name = models.CharField(max_length=128)
 
-class Resena(models.Model):
-    texto = models.TextField()
-    calificacion = models.IntegerField()
-    pelicula = models.ForeignKey(Pelicula,on_delete=models.CASCADE)   
-    usuario = models.ForeignKey(Usuario,on_delete=models.CASCADE)   
+    def __str__(self):
+        return self.name
 
-#class Seguidor(models.Model):
-#    usuario = models.ForeignKey(Usuario,on_delete=models.CASCADE)   
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    overview = models.TextField()
+    release_date = models.DateTimeField()
+    running_time = models.IntegerField()
+    budget = models.IntegerField(blank=True)
+    tmdb_id = models.IntegerField(blank=True, unique=True)
+    revenue = models.IntegerField(blank=True)
+    poster_path = models.URLField(blank=True)
+    genres = models.ManyToManyField(Genre)
+    credits = models.ManyToManyField(Person, through="MovieCredit")
+
+    def __str__(self):
+        return self.title
+
+
+class MovieCredit(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+
+
+class MovieReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1),
+                                                          MaxValueValidator(100)])
+    review = models.TextField(blank=True)
